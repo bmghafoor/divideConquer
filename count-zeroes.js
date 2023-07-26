caa24287abd486f5811850fe733812072e79eca4
Binyameen <bmghafoor@gmail.com>
2023-07-26 16:52:18 -0400

    Count zeros

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,28 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+  if (arr[arr.length - 1] !== 0) {
+    return 0;
+  }
+  if (arr[0] === 0) {
+    return arr.length;
+  }
+  let leftIdx = 0;
+  let rightIdx = arr.length - 1;
+  let found = false;
+  while (leftIdx <= rightIdx) {
+    let middleIdx = Math.floor((rightIdx + leftIdx) / 2);
+    console.log(middleIdx);
+    if (arr[middleIdx] == 1 && arr[middleIdx + 1] == 0) {
+      found = true;
+      return arr.length - 1 - middleIdx;
+    } else if (arr[middleIdx] == 1 && arr[middleIdx + 1] == 1) {
+      leftIdx = middleIdx + 1;
+    } else if (arr[middleIdx] == 0 && arr[middleIdx - 1] == 1) {
+      found = true;
+      return arr.length - middleIdx;
+    } else if (arr[middleIdx] == 0 && arr[middleIdx - 1] == 0) {
+      rightIdx = middleIdx - 1;
+    }
+  }
 }
 
-module.exports = countZeroes
+module.exports = countZeroes;
