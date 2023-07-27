a081615f657a609d4561a5fb109b106e134243ce
Binyameen <bmghafoor@gmail.com>
2023-07-27 13:43:12 -0400

    rotated array

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,25 @@
-function findRotatedIndex() {
- 
+function findRotatedIndex(arr, val) {
+  let leftIdx = 0;
+  let rightIdx = arr.length - 1;
+  while (leftIdx <= rightIdx) {
+    // edge case for if it's first idx
+    if (arr[leftIdx] === val) {
+      return leftIdx;
+    }
+    // edge case for if its last val
+    if (arr[rightIdx] === val) {
+      return rightIdx;
+    }
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    let middleVal = arr[middleIdx];
+    if (middleVal === val) {
+      return middleIdx;
+    } else if (middleVal !== val) {
+      leftIdx += 1;
+      rightIdx -= 1;
+    }
+  }
+  return -1;
 }
 
-module.exports = findRotatedIndex
+module.exports = findRotatedIndex;
