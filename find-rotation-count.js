f5c0375cbf768d62bdd5d1bf1b08cd960f019e60
Binyameen <bmghafoor@gmail.com>
2023-07-27 17:03:17 -0400

    rotated count

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,34 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+  if (arr[arr.length - 1] > arr[0]) {
+    return 0;
+  }
+
+  let leftIdx = 0;
+  let rightIdx = arr.length - 2;
+  let lastVal = arr[arr.length - 1];
+  if (lastVal < arr[rightIdx]) {
+    return arr.length - 1;
+  }
+  while (leftIdx <= rightIdx) {
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    let middleVal = arr[middleIdx];
+    if (lastVal > middleVal) {
+      console.log("case 1");
+      rightIdx = middleIdx - 1;
+    } else if (lastVal < middleVal) {
+      console.log("case 2");
+      if (arr[middleIdx + 1] < lastVal) {
+        return middleIdx + 1;
+      }
+      leftIdx = middleIdx + 1;
+      if (middleIdx === 0) {
+        return rightIdx + 1;
+      }
+    } else if (lastVal < middleVal && lastVal < arr[middleIdx - 1]) {
+      console.log("case 3");
+      return middleIdx;
+    }
+  }
 }
 
-module.exports = findRotationCount
+module.exports = findRotationCount;
--- find-rotation-count.test.js
+++ find-rotation-count.test.js
@@ -1,9 +1,12 @@
-const findRotationCount = require("./find-rotation-count")
+const findRotationCount = require("./find-rotation-count");
 
 describe("#findRotationCount", function () {
   it("returns the number of rotations", function () {
-    expect(findRotationCount([15, 18, 2, 3, 6, 12])).toBe(2)
-    expect(findRotationCount([7, 9, 11, 12, 5])).toBe(4)
-    expect(findRotationCount([7, 9, 11, 12, 15])).toBe(0)
-  })
-})
+    expect(findRotationCount([15, 18, 2, 3, 6, 12])).toBe(2);
+    expect(findRotationCount([15, 18, 2, 3, 4, 5, 6, 7, 12])).toBe(2);
+    expect(findRotationCount([7, 9, 11, 12, 5])).toBe(4);
+    expect(findRotationCount([7, 9, 11, 12, 4, 5])).toBe(4);
+    expect(findRotationCount([7, 9, 11, 12, 15])).toBe(0);
+    expect(findRotationCount([13, 12, 11, 10, 9, 8, 1, 2, 3, 4, 5, 6])).toBe(6);
+  });
+});
