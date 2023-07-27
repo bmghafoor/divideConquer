838c97f9ef6800fbea3fa3d8519111b0dd950856
Binyameen <bmghafoor@gmail.com>
2023-07-27 13:15:27 -0400

    fixing code

--- count-zeroes.js
+++ count-zeroes.js
@@ -7,17 +7,13 @@ function countZeroes(arr) {
   }
   let leftIdx = 0;
   let rightIdx = arr.length - 1;
-  let found = false;
   while (leftIdx <= rightIdx) {
     let middleIdx = Math.floor((rightIdx + leftIdx) / 2);
-    console.log(middleIdx);
     if (arr[middleIdx] == 1 && arr[middleIdx + 1] == 0) {
-      found = true;
       return arr.length - 1 - middleIdx;
     } else if (arr[middleIdx] == 1 && arr[middleIdx + 1] == 1) {
       leftIdx = middleIdx + 1;
     } else if (arr[middleIdx] == 0 && arr[middleIdx - 1] == 1) {
-      found = true;
       return arr.length - middleIdx;
     } else if (arr[middleIdx] == 0 && arr[middleIdx - 1] == 0) {
       rightIdx = middleIdx - 1;
