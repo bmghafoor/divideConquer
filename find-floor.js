e290bf58d85b823109f34717a45ca76d6795bba6
Binyameen <bmghafoor@gmail.com>
2023-07-27 17:21:44 -0400

    find floor

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,24 @@
-function findFloor() {
-  
+function findFloor(arr, val) {
+  if (val === 0) {
+    return -1;
+  }
+  let leftIdx = 0;
+  let rightIdx = arr.length - 1;
+  if (arr[rightIdx] < val) {
+    return arr[rightIdx];
+  }
+  while (leftIdx <= rightIdx) {
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    let middleVal = arr[middleIdx];
+    if (middleVal > val) {
+      rightIdx = middleIdx - 1;
+      if (arr[rightIdx] < val) {
+        return arr[rightIdx];
+      }
+    } else if (middleVal < val) {
+      leftIdx = middleIdx + 1;
+    }
+  }
 }
 
-module.exports = findFloor
+module.exports = findFloor;
